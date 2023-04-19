3a3728f2f380de342b9c57620165a43ce9474059
dev-com2022 <kwp@tlen.pl>
2023-04-19 10:29:38 +0200

    19.04 10:30

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 let app = express();
+app.use(express.static(__dirname + "/public"));
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + ' \n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
@@ -10,8 +23,7 @@ app.get("/", (req, res) => {
 app.get("/person", (req, res) => {
     res.render('person.hbs', {
         pageTitle: 'Person Page',
-        message: 'Hello there!',
-        currentYear: new Date().getFullYear(),
+        message: 'Hello there!'
     });
   });
   
@@ -26,9 +38,18 @@ app.get("/bad", (req, res) => {
   });
 
 });
-app.use(express.static(__dirname + "/public"));
+
+
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
-app.listen(3000, ()=> {
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+    }); 
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+    });
+
+    app.listen(3000, ()=> {
     console.log("Serwer działa na porcie 3000");
 });
